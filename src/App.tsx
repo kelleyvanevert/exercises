eeb40025b8edb9628e7ccb0334ff785cc9c22c19
Kelley van Evert <kelley@codaisseur.com>
2019-04-05 21:05:30 +0200

    Some app-component level UI additions

--- src/App.tsx
+++ src/App.tsx
@@ -9,50 +9,36 @@ const renderers = {
   eval: makeExerciseRenderer(simpleCodeEval)
 };
 
-const exercises = [
-  {
-    type: "md_multi",
-    question: "Which of the following is most true?",
-    options: [
-      "Markdown is wonderful",
-      "TypeScript is great",
-      "Generic types are hard"
-    ],
-    correctIndex: 1
-  },
-  {
-    type: "md_multi",
-    question: "Which of the following is most true?",
-    options: [
-      "Markdown is wonderful",
-      "TypeScript is great",
-      "Generic types are hard"
-    ],
-    correctIndex: 1
-  },
-  {
-    type: "eval",
-    description: "Compute the number 42",
-    correctResult: 42
-  }
-];
+interface IExercise {
+  type: string;
+}
 
 interface IState {
   at: number;
   progress: Array<{
-    exercise: { type: string };
+    exercise: IExercise;
     savedAnswer?: unknown;
   }>;
+  loading: boolean;
 }
 
 class App extends React.Component<{}, IState> {
   state: IState = {
     at: 0,
-    progress: exercises.map(exercise => ({
-      exercise,
-      passed: false
-    }))
+    progress: [],
+    loading: true
   };
+
+  async componentDidMount() {
+    const response = await fetch("/exercises.json");
+    const exercises = await response.json();
+    this.setState({
+      loading: false,
+      progress: exercises.map((exercise: IExercise) => ({
+        exercise
+      }))
+    });
+  }
 
   onSave = (answer: unknown) => {
     this.setState({
@@ -68,24 +54,55 @@ class App extends React.Component<{}, IState> {
       at: at + 1
     });
   };
-
+  /*
+  startOver = () => {
+    this.setState({
+      progress: this.state.progress.map(({ exercise }) => ({ exercise }))
+    });
+  };
+  */
   render() {
-    const { at, progress } = this.state;
+    const { loading, at, progress } = this.state;
 
     return (
       <div>
-        {progress.slice(0, at + 1).map(({ exercise, savedAnswer }, i) => {
-          const ExerciseRenderer = renderers[exercise.type];
-          return (
-            <ExerciseRenderer
-              key={i}
-              exercise={exercise}
-              savedAnswer={savedAnswer}
-              onPass={this.onPass}
-              onSave={this.onSave}
-            />
-          );
-        })}
+        <h2>Exercises</h2>
+        <p>
+          These exercises are only meant for you: to practice and gain
+          understanding through it. They are not evaluated formally. You may
+          choose to practice as little or often as you think is necessary. Your
+          answers are automatically saved, so you don't have to worry about
+          losing them.
+        </p>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {progress.slice(0, at + 1).map(({ exercise, savedAnswer }, i) => {
+              const ExerciseRenderer = renderers[exercise.type];
+              return (
+                <li key={i}>
+                  <div>
+                    Exercise {i + 1} / {progress.length}
+                  </div>
+                  <ExerciseRenderer
+                    exercise={exercise}
+                    savedAnswer={savedAnswer}
+                    onPass={this.onPass}
+                    onSave={this.onSave}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {at === progress.length ? (
+          <p>
+            <strong>Congratulations!</strong> You made it through all the
+            exercises. This also means that we're quite confident you understand
+            this topic well.
+          </p>
+        ) : null}
       </div>
     );
   }
